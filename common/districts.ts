2b01d02b305ec9f5cba08330f01a29562aed9802
ninedev <master-vva@narod.ru>
2024-08-09 17:22:51 +0300

    rename region to SPb

--- common/districts.ts
+++ common/districts.ts
@@ -3,7 +3,7 @@ import type { Districts } from '~/common/types';
 export const districts: Districts = {
   ru: [
     { value: 'moscow', label: 'Москва и область', coordinates: [55.8, 37.5], zoom: 9 },
-    { value: 'spb', label: 'Северо-Западный округ', coordinates: [59.75, 30.5], zoom: 9 },
+    { value: 'spb', label: 'Санкт-Петербург', coordinates: [59.75, 30.5], zoom: 9 },
     { value: 'center', label: 'Центральный округ', coordinates: [54.21, 37.62], zoom: 12 },
     { value: 'volga', label: 'Приволжский округ', coordinates: [57.5, 51], zoom: 6 },
     { value: 'ural', label: 'Уральский округ', coordinates: [56.84, 60.57], zoom: 12 },
