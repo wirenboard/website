77286d1636e137bbb2e1f84aed5efd1d75999168
ninedev <master-vva@narod.ru>
2025-01-22 14:55:51 +0300

    add include component

---
+++ components/content/Include.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{ path: string; }>();
+
+const { data } = await useAsyncData(props.path, () => queryContent(props.path).findOne());
+</script>
+
+<template>
+  <ContentRenderer v-if="data" :value="data" />
+</template>
+
