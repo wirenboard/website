cee1424fac004f771e6eafc5ac3a7dfd57f65654
Dmitriy Ustalov <47390556+dust70@users.noreply.github.com>
2025-01-28 15:07:16 +0400

    Сменил название категории (#125)

--- common/solution_categories.ts
+++ common/solution_categories.ts
@@ -6,7 +6,7 @@ export const categories: Categories = {
     { name: 'building_dispatching', label: 'Диспетчеризация многоквартирных домов' },
     { name: 'commercial_estate_monitoring', label: 'Мониторинг и автоматизация коммерческой недвижимости' },
     { name: 'server_monitoring', label: 'Мониторинг серверного оборудования и базовых станций' },
-    { name: 'industrial_dispatching_monitoring', label: 'Диспетчеризация и мониторинг промышленных объектов' },
+    { name: 'industrial_dispatching_monitoring', label: 'Промышленная автоматизация и диспетчеризация' },
     { name: 'agriculture_automation', label: 'Автоматизация объектов сельского хозяйства' },
     { name: 'embedded_systems', label: 'Встраиваемые системы' },
     { name: 'apartments_automation', label: 'Автоматизация частных квартир и домов' },
