16f994ab896898ad334945b094613b03d12e898b
DenisN <denis.nikolaev@wirenboard.com>
2025-04-03 08:37:41 +0300

    Update solution_categories.ts

--- common/solution_categories.ts
+++ common/solution_categories.ts
@@ -17,7 +17,7 @@ export const categories: Categories = {
     { name: 'commercial_estate_monitoring', label: 'Monitoring and automation of commercial real estate' },
     //{ name: 'server_monitoring', label: 'Monitoring of server equipment and base stations' },
     { name: 'industrial_dispatching_monitoring', label: 'Dispatching and monitoring of industrial facilities' },
-    //{ name: 'agriculture_automation', label: 'Automation of agricultural facilities' },
+    { name: 'agriculture_automation', label: 'Automation of agricultural facilities' },
     //{ name: 'embedded_systems', label: 'Embedded systems' },
     { name: 'apartments_automation', label: 'Automation of private apartments and houses' },
   ],
