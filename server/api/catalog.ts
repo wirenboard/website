f41c0ddeb606152cc78fa6a2bb721957ea29b0c9
Victor Vedenin <master-vva@narod.ru>
2025-04-08 14:31:52 +0300

    add catalog endpoint (#180)

---
+++ server/api/catalog.ts
@@ -0,0 +1,20 @@
+import { serverQueryContent } from '#content/server';
+import { toHtml } from 'hast-util-to-html';
+import { toHast } from 'mdast-util-to-hast';
+
+export default defineEventHandler(async (event) => {
+  const products = await serverQueryContent(event)
+    .where({ _path: { $contains: '_catalog' } })
+    .find();
+
+  return products.reduce((acc: any, product) => {
+    // @ts-ignore
+    const hast = toHast(product.body.children[0].children.find(item => Object.hasOwn(item.props, 'v-slot:description')));
+
+    acc[product.article] = {
+      cover: `https://wirenboard.com/_ipx/h_120&q_70&f_webp/${product.cover}`,
+      description: toHtml(hast, { allowDangerousHtml: true })
+    }
+    return acc;
+  }, {});
+});
