4324e4c5dc87ed6e5998faeb3adf408c95119c72
ninedev <master-vva@narod.ru>
2025-07-07 15:58:21 +0300

    add localization for includes

--- components/content/Include.vue
+++ components/content/Include.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 const props = defineProps<{ path: string; }>();
+const { locale } = useI18n();
 
-const { data } = await useAsyncData(props.path, () => queryContent(props.path).findOne());
+const { data } = await useAsyncData(props.path, () => queryContent(props.path).where({ _locale: locale.value }).findOne());
 </script>
 
 <template>
