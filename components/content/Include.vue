8fc7de996064658bbbf034a8e5310ba5a49b1a56
ninedev <master-vva@narod.ru>
2025-09-11 17:54:03 +0300

    fix include image url

--- components/content/Include.vue
+++ components/content/Include.vue
@@ -3,6 +3,27 @@ const props = defineProps<{ path: string; }>();
 const { locale } = useI18n();
 
 const { data } = await useAsyncData(props.path, () => queryContent(props.path).where({ _locale: locale.value }).findOne());
+
+const includePath = data.value!._path?.substring(0, data.value!._path.lastIndexOf('/'));
+
+// includes can contain images and it's url is incorrect, because photo component didn't know about include
+const updateImagePath = (obj: any) => {
+  if (obj.tag === 'photo') {
+    const src = obj.props.src.replace(/^(?:[^/]+\/)?(?:catalog|articles|integrators|jobs|partners|solutions|video)\//, '');
+
+    // check is it contains included path to prevent reassign url on rehydration
+    if (!obj.props.src.includes(includePath)) {
+      obj.props.src = `${locale.value}${includePath}/${src}`;
+    }
+  }
+  if (obj.children && obj.children.length) {
+    obj.children.forEach((child: any) => {
+      updateImagePath(child);
+    })
+  }
+};
+
+updateImagePath(data.value?.body);
 </script>
 
 <template>
