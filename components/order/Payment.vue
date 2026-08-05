d7180b74c3a30ca275b2212220bb91725597e7bf
Ekaterina Volkova <97599621+ekateluv@users.noreply.github.com>
2026-08-05 14:47:14 +0300

    Warn about browser restrictions when card payment is selected (#515)

--- components/order/Payment.vue
+++ components/order/Payment.vue
@@ -36,6 +36,17 @@ watch(paymentMethods, (methods) => {
       name="payment"
       :items="paymentTypes"
     />
+
+    <p v-if="paymentType === 'card'" class="order-paymentWarning">
+      <i18n-t keypath="cardWarning">
+        <template #link>
+          <a href="https://ria.ru/20260804/brauzery-2108832797.html" target="_blank" rel="noopener">{{ t('cardWarningLink') }}</a>
+        </template>
+        <template #certLink>
+          <a href="https://www.gosuslugi.ru/crt" target="_blank" rel="noopener">{{ t('cardWarningCertLink') }}</a>
+        </template>
+      </i18n-t>
+    </p>
   </div>
 </template>
 
@@ -43,6 +54,21 @@ watch(paymentMethods, (methods) => {
 .order-payment {
   margin-top: 56px;
   margin-bottom: 48px;
+}
+
+.order-paymentWarning {
+  margin-top: 18px;
+  padding: 14px 18px;
+  border: 1px solid #f0c36d;
+  border-radius: 2px;
+  background: #fdf7e6;
+  font-size: 20px;
+  line-height: 1.4em;
+  color: var(--text-color);
+}
+
+.order-paymentWarning a {
+  color: var(--link-color);
 }
 </style>
 
@@ -55,7 +81,10 @@ watch(paymentMethods, (methods) => {
     "card": "Банковской картой",
     "cardComment": "Visa / Mastercard / МИР",
     "requisites": "Банковским переводом",
-    "requisitesComment": "По реквизитам"
+    "requisitesComment": "По реквизитам",
+    "cardWarning": "К сожалению, из-за отзыва сертификатов {link}. Для того чтобы оплатить заказ по карте, выберите на следующем шаге оплату по QR-коду, или установите в своем браузере {certLink}, или оформите заказ в Яндекс Браузере. Приносим свои извинения, мы сделаем все возможное для решения проблемы.",
+    "cardWarningLink": "некоторые браузеры ограничивают доступ к оплате по карте",
+    "cardWarningCertLink": "сертификат Минцифры"
   },
   "en": {
     "title": "Select a payment method",
@@ -64,7 +93,10 @@ watch(paymentMethods, (methods) => {
     "card": "By bank card",
     "cardComment": "Visa / Mastercard / MIR",
     "requisites": "By bank transfer",
-    "requisitesComment": "Using bank details"
+    "requisitesComment": "Using bank details",
+    "cardWarning": "Unfortunately, due to certificate revocation, {link}. To pay for your order by card, select QR code payment at the next step, or install the {certLink} in your browser, or place your order using Yandex Browser. We apologize for the inconvenience and are doing our best to resolve the issue.",
+    "cardWarningLink": "some browsers restrict access to card payments",
+    "cardWarningCertLink": "Ministry of Digital Development certificate"
   }
 }
 </i18n>
